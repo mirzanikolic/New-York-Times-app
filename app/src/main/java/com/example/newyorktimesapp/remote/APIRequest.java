67762f9ad50fe5cc535a203c4494d780811689f6
Mirza Nikolic <mirza.nikolic@gmail.com>
2022-07-14 00:36:45 +0200

    Add API request and utilities

---
+++ app/src/main/java/com/example/newyorktimesapp/remote/APIRequest.java
@@ -0,0 +1,20 @@
+package com.example.newyorktimesapp.remote;
+
+import com.example.newyorktimesapp.models.Doc;
+import com.example.newyorktimesapp.models.Example;
+import com.example.newyorktimesapp.models.Response;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface APIRequest {
+    String BASE_URL = "https://api.nytimes.com/svc/";
+    String API_IMAGE_BASE_URL = "https://www.nytimes.com/";
+
+    @GET("search/v2/articlesearch.json")
+    Call<Example> getSearch(
+            @Query("q") String keyword,
+            @Query("api-key") String apiKey
+    );
+}
---
+++ app/src/main/java/com/example/newyorktimesapp/remote/ApiUtils.java
@@ -0,0 +1,18 @@
+package com.example.newyorktimesapp.remote;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiUtils {
+    private static Retrofit retrofit = null;
+
+    public static APIRequest getApiRequest() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(APIRequest.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(APIRequest.class);
+    }
+}
