364bc83066187610a6eced9fe3abaad7bdf54c91
Mirza Nikolic <mirza.nikolic@gmail.com>
2022-07-14 00:50:54 +0200

    Fix errors, add new design pattern

--- app/src/main/java/com/example/newyorktimesapp/MainActivity.java
+++ app/src/main/java/com/example/newyorktimesapp/MainActivity.java
@@ -4,21 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.Adapter;
 
 import com.example.newyorktimesapp.models.Doc;
-import com.example.newyorktimesapp.remote.APIRequest;
-import com.example.newyorktimesapp.remote.ApiClient;
+import com.example.newyorktimesapp.models.Example;
 import com.example.newyorktimesapp.remote.ApiUtils;
 
+
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class MainActivity extends AppCompatActivity {
     public final String apiKey = "tZ9QFoQl6FCLFr6IgrIKX8aSI2IYxt1Y";
     NewsAdapter adapter;
     ArrayList<Doc> news;
-    String q = "Biden";
+    String q = "Trump";
 
 
     @Override
@@ -36,10 +39,24 @@ public class MainActivity extends AppCompatActivity {
         getNews();
 
 
-
     }
 
-    public void getNews()
+    public void getNews(){
+        ApiUtils.getApiRequest().getSearch(q,apiKey).enqueue(new Callback<Example>() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onResponse(Call<Example> call, retrofit2.Response<Example> response) {
+                if(response.isSuccessful()){
+                    assert response.body() != null;
+                    news.addAll(response.body().getResponse().getDocs());
+                    adapter.notifyDataSetChanged();
+                }
+            }
 
+            @Override
+            public void onFailure(Call<Example> call, Throwable t) {
+            }
+        });
+    }
 
 }
