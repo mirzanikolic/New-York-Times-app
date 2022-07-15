df76a78eedca47204b3005b748598af999853f56
Mirza Nikolic <mirza.nikolic@gmail.com>
2022-07-15 17:42:44 +0200

    Add webView to display data from New York Times api

--- app/src/main/java/com/example/newyorktimesapp/DetailsActivity.java
+++ app/src/main/java/com/example/newyorktimesapp/DetailsActivity.java
@@ -2,23 +2,46 @@ package com.example.newyorktimesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
+import javax.security.auth.callback.Callback;
+
 public class DetailsActivity extends AppCompatActivity {
-    TextView textView;
+    WebView webView;
     String webUrl;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        textView = findViewById(R.id.text_view);
+
+        webView = findViewById(R.id.news_web_view);
+
         Bundle extras = getIntent().getExtras();
-        if(extras!=null){
+        if(extras!=null) {
             webUrl = extras.getString("newsUrl");
-            textView.setText(webUrl);
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webView.setWebViewClient(new CallBack());
+            webView.loadUrl(webUrl);
+
         }
 
+    }
+
+    private static class CallBack extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
     }
 }
